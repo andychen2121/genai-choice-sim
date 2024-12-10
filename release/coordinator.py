dd552328b0b54b5d73e6ef979ed6db7bc888efdf
andychen2121 <andychen2121@berkeley.edu>
2024-12-09 18:19:00 -0800

    QOL + coordinator infra

--- release/coordinator.py
+++ release/coordinator.py
@@ -1,5 +1,30 @@
 import json
+import os
 from LLM_plot_gen import create_story
+
+
+class Coordinator:
+    def __init__(self):
+        pass
+    
+    # statically generate first N layers of storyline
+    def initialize_storyline(self, IP):
+        if not os.path.exists(f"{IP}.json"):
+            self.story_json = create_story(IP)
+            with open(f"{IP}.json", "w") as f:
+                json.dump(self.story_json, f)
+        else:
+            self.story_json = json.loads(f"{IP}.json")
+    
+    # TBD
+    # gets next node; if DNE, dynamically generate next node for given story
+    def continue_story(self, current_label, next_label):
+        if current_label + next_label in self.story_json:
+            return self.story_json[current_label + next_label]
+        else:
+            # TODO: dynamic generation
+            pass
+
 
 if __name__ == '__main__':
     ex_json = create_story('League of Legends - Arcane')
